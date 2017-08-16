8c804e4b21b084f611a096eae835c939a6be9e7c
Jay Sullivan <JaySull514@gmail.com>
2017-08-16 18:55:48 -0500

    styling

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,11 +1,17 @@
 $(window).scroll(function() {
-    $('.nav-bar').toggleClass('scrolled', $(this).scrollTop() > 1)
+    $('.nav-bar').toggleClass('scrolled', $(this).scrollTop() > 525)
 });
 
 $(window).scroll(function() {
-    $('.nav-search').toggleClass('nav-scrolled', $(this).scrollTop() > 1)
+    $('.nav-search').toggleClass('nav-scrolled', $(this).scrollTop() > 525)
 });
 
 $(window).scroll(function() {
-    $('.nav-title').toggleClass('nav-title-scrolled', $(this).scrollTop() > 1)
+    $('.nav-title').toggleClass('nav-title-scrolled', $(this).scrollTop() > 525)
+});
+
+$(window).scroll(function() {
+    $('.shadow-wrapper').css({
+        opacity: ($(this).scrollTop() / 900)
+    });
 });
