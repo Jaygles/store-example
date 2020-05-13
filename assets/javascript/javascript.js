cc0f32ac5321e5688542e9b29d1bfb9da258b9fd
Jay Sullivan <JaySull514@gmail.com>
2020-05-12 20:03:43 -0600

    Remove jquery

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,17 +1,43 @@
-$(window).scroll(function() {
-    $('.nav-bar').toggleClass('scrolled', $(this).scrollTop() > 525)
-});
+const shadowWrapperEl = document.getElementsByClassName("shadow-wrapper")[0];
 
-$(window).scroll(function() {
-    $('.nav-search').toggleClass('nav-scrolled', $(this).scrollTop() > 525)
-});
+function doSomething(scrollTop) {
+    shadowWrapperEl.style.opacity = scrollTop / 900;
+};
 
-$(window).scroll(function() {
-    $('.nav-title').toggleClass('nav-title-scrolled', $(this).scrollTop() > 525)
-});
+let ticking = false;
 
-$(window).scroll(function() {
-    $('.shadow-wrapper').css({
-        opacity: ($(this).scrollTop() / 900)
-    });
-});
+window.addEventListener('scroll', function(e) {
+    scrollTop = window.scrollY;
+
+    if (!ticking) {
+      window.requestAnimationFrame(function() {
+        doSomething(scrollTop);
+        ticking = false;
+      });
+
+      ticking = true;
+    }
+  });
+
+const navBarEl = document.getElementsByClassName("nav-bar")[0];
+const navSearchEl = document.getElementsByClassName("nav-search")[0];
+const navTitleEl = document.getElementsByClassName("nav-title")[0];
+
+function toggleClasses() {
+    setTimeout(() => {
+        if (window.scrollY < 525) {
+            navBarEl.classList.remove("scrolled");
+            navSearchEl.classList.remove("scrolled");
+            navTitleEl.classList.remove("scrolled");
+        } else {
+            navBarEl.classList.add("scrolled");
+            navSearchEl.classList.add("scrolled");
+            navTitleEl.classList.add("scrolled");
+        }
+
+        toggleClasses();
+    }, 333);
+}
+
+toggleClasses();
+
